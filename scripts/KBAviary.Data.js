e97c8307d38029c6b8dcc59bbd96457fb8fb7bca
Diana Grams <deegrams221@gmail.com>
2020-10-06 13:25:34 -0600

    added indexes

--- scripts/KBAviary.Data.js
+++ scripts/KBAviary.Data.js
@@ -1,37 +1,47 @@
 'use strict';
 
-KBAviary.prototype.addbird = function (data) {
-  /*
-    TODO: Implement adding a document
-  */
+KBAviary.prototype.addBird = function(data) {
+  var collection = firebase.firestore().collection('inventory');
+  return collection.add(data);
 };
 
-KBAviary.prototype.getAllbirds = function (renderer) {
-  /*
-    TODO: Retrieve list of birds
-  */
+KBAviary.prototype.getAllBirds = function(renderer) {
+  var query = firebase
+    .firestore()
+    .collection('inventory')
+    .orderBy('type', 'desc')
+    .limit(50);
+  this.getDocumentsInQuery(query, renderer);
 };
 
-KBAviary.prototype.getDocumentsInQuery = function (query, renderer) {
-  /*
-    TODO: Render all documents in the provided query
-  */
+KBAviary.prototype.getDocumentsInQuery = function(query, renderer) {
+  query.onSnapshot(function(snapshot) {
+    if (!snapshot.size) return renderer.empty();
+
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        renderer.remove(change.doc);
+      } else {
+        renderer.display(change.doc);
+      }
+    });
+  });
 };
 
-KBAviary.prototype.getbird = function (id) {
-  /*
-    TODO: Retrieve a single bird
-  */
+KBAviary.prototype.getBird = function(id) {
+  return firebase
+    .firestore()
+    .collection('inventory')
+    .doc(id)
+    .get();
 };
 
-KBAviary.prototype.getFilteredbirds = function (filters, renderer) {
-  /*
-    TODO: Retrieve filtered list of birds
-  */
-};
+KBAviary.prototype.getFilteredBirds = function(filters, renderer) {
+  var query = firebase.firestore().collection('inventory');
 
-KBAviary.prototype.addRating = function (birdID, rating) {
-  /*
-    TODO: Retrieve add a rating to a bird
-  */
+  if (filters.type !== 'Any') {
+    query = query.where('type', '==', filter.type);
+  }
+
+  this.getDocumentsInQuery(query, renderer);
 };
